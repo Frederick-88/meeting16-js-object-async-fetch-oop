34f0cfac674ee5fd21140bd1e2e220545e5b1c71
Frederick-88 <frederickfd88@gmail.com>
2020-04-07 10:08:40 +0700

    update

---
+++ script.js
@@ -0,0 +1,54 @@
+// Function dalam Object
+// THIS NAME = MANUSIA.NAME, DISRANKAN PKAI MANUSIA.NAME
+const manusia = {
+    name: 'frederick',
+    lastName: 'Gokil',
+    getFullName(){
+        return (`${this.name} ${this.lastName}`)
+    },
+    changeName(namaYangDiubah){
+        this.name = namaYangDiubah;
+    },
+    setAnak(...args){
+        let anakArray = [];
+        for(let anak of args){
+            anakArray.push(anak);
+        }
+        this.anak = anakArray;
+    },
+    getAnak(){
+        return this.anak;
+    }
+}
+console.log(manusia.getFullName());
+
+// atau
+
+let namaLengkap = manusia.getFullName();
+console.log(namaLengkap);
+
+manusia.changeName('GANTI NAMA');
+namaLengkap = manusia.getFullName();
+console.log(namaLengkap);
+
+// DATE FUNCTION
+const date = new Date();
+
+const tanggal = (`Tanggal hari ini adalah ${date.getDate()} / ${date.getMonth() +1} / ${date.getFullYear()}`);
+console.log(tanggal);
+// 
+
+// DESTRUCTURE
+// PENAMAAN LAMA
+// const name = manusia.name;
+// const lastName = manusia.lastName;
+
+// DESTRUCTURE PENAMAAN BARU - DGN SYARAT ELEMEN YANG SAMA
+const {name, lastName} = manusia;
+console.log(`${name} ${lastName}`);
+
+    // LEARN DOM EVENT LISTENERS
+    document.querySelector('#clickMe').addEventListener('click',()=>{
+        document.create
+        // window.alert('HAI!!')
+    })
